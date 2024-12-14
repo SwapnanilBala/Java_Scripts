97236f9bd4122c9b794f2e7bfd3a1d5e126ae8a1
Your Name <your_email@example.com>
2024-12-14 20:22:25 +0530

    add some .js files to test

---
+++ variables.js
@@ -0,0 +1,23 @@
+const account_id = 123412
+let account_email = "arranfrost007@gmail.com"
+var account_password = "232124"
+account_city = "Kolkata"
+
+
+// account_id = 3425 // not allowed
+
+account_email = "gogoll242@gmail.com"
+account_password = "35992"
+account_city = "Karnatak"
+
+console.log(account_id);
+
+console.table([account_id,account_email,account_password,account_city])
+
+/*
+
+This is the comment section, dont use Var
+
+*/
+
+console.log(typeof(account_password));
