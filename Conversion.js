afba0e3807627741cc869dd52c8038dfef28778c
Your Name <your_email@example.com>
2024-12-16 14:12:29 +0530

    added a bunch of new lines

--- Conversion.js
+++ Conversion.js
@@ -7,7 +7,37 @@
 // Workable_Value = Number(score)
 // console.log(typeof(Workable_Value))
 
-let boolean_value = "Raftaar"
-let value = Boolean(boolean_value)
+// let boolean_value = "Raftaar"
+// let value = Boolean(boolean_value)
 
-console.log(value)
+// console.log(value)
+
+// // ************* Operations Baby ************
+
+// let str_1 = "Hello"
+// let Str_2 = "Gogol"
+// let str_3 = (str_1 + " " + Str_2)
+
+// console.log(str_3)
+// console.log("")
+
+// let a = "1"
+// let b = 2
+// arma = ""
+// Boolean_Value = Boolean(arma)
+
+// let c = Boolean_Value
+// console.log(c)
+// console.log("")
+// console.log(a,b,c)
+// console.log(a+b+c)
+
+// console.log(true) will return true
+// however console.log(+true) will return 1 
+
+// console.log(+false)
+
+let number_counter = 50
+++number_counter
+number_counter++
+console.log(number_counter)
