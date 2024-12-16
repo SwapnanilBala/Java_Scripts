62113e3e7042de68c24f1f6e906bd7027934f5ec
Your Name <your_email@example.com>
2024-12-16 13:41:07 +0530

    added some more lines

---
+++ Conversion.js
@@ -0,0 +1,13 @@
+// let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// console.log(" ")
+
+// Workable_Value = Number(score)
+// console.log(typeof(Workable_Value))
+
+let boolean_value = "Raftaar"
+let value = Boolean(boolean_value)
+
+console.log(value)
